22b02c3716e2d24ee155f6504319b94b544ca91c
Ferdousara Parvin <parvin.ferdousara@gmail.com>
2018-03-28 15:26:13 -0400

    here is the code

---
+++ Assignment4/src/assignment4/Course.java
@@ -0,0 +1,106 @@
+package assignment4;
+
+import java.util.Scanner;
+
+public class Course implements DirectlyRelatable{
+    
+    private String courseID;
+    private String courseName; // Always a single word
+    private double credit;
+    private String preReqID;
+    private String coReqID;
+    
+    private Scanner kb;
+
+    // Parameterized constructor 
+    public Course(String courseID, String courseName, double credit, String preReqID, String coReqID) {
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.credit = credit;
+        this.preReqID = preReqID;
+        this.coReqID = coReqID;
+    }
+    
+    // Copy constructor
+    public Course(Course other, String value)
+    {
+        
+    }
+    
+    public Object clone()
+    {
+        kb = new Scanner(System.in);
+        System.out.println("Please enter the courseID of the course you wish to clone");
+        
+        return Object;
+    }
+    
+    // Implement method from interface
+    public boolean isDirectlyRelatable(Course c)
+    {
+        
+    }
+
+    // Equals method
+    @Override
+    public boolean equals(Object obj) {
+        if ((this == null) || (obj == null) || (this.getClass() != obj.getClass())) {
+            return false;
+        }
+
+        Course other = (Course) obj;
+        
+        return (this.credit == other.credit && this.courseName == other.courseName && this.preReqID == other.preReqID && this.coReqID == other.coReqID);
+    
+    }
+
+    // ToString method
+    @Override
+    public String toString() {
+        return "Course{" + "courseID=" + courseID + ", courseName=" + courseName + ", credit=" + credit + ", preReqID=" + preReqID + ", coReqID=" + coReqID + '}';
+    }
+
+    // Accessors and mutators
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    public String getPreReqID() {
+        return preReqID;
+    }
+
+    public void setPreReqID(String preReqID) {
+        this.preReqID = preReqID;
+    }
+
+    public String getCoReqID() {
+        return coReqID;
+    }
+
+    public void setCoReqID(String coReqID) {
+        this.coReqID = coReqID;
+    }
+    
+    
+
+}
---
+++ Assignment4/src/assignment4/DirectlyRelatable.java
@@ -0,0 +1,7 @@
+package assignment4;
+
+public interface DirectlyRelatable{
+    
+    public boolean isDirectlyRelatable(Course C);
+
+}
